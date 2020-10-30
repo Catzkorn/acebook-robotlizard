980bf6c1eec41c4292721738bb023507c361fdcd
Xavier Defontaine <xdefontaine@gmail.com>
2020-10-30 14:37:29 +0000

    fixed avatar and send emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,9 +33,21 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+ 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { :host => "https://acebook-robotlizard.herokuapp.com" }
+  config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: "robotlizard2020@gmail.com",
+    password: ENV["GMAIL_PASSWORD"],
+    # domain: "localhost:3000",
+    address: "smtp.gmail.com",
+    port: "587",
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
