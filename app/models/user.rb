8071dec04809e9e4efa2534784e8f72d0d64a579
Xavier Defontaine <xdefontaine@gmail.com>
2020-11-02 10:50:13 +0000

    fixed the likes destroy bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_friendship
-  acts_as_voter
+  has_friendship 
+  acts_as_voter dependent: :destroy
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  has_one_attached :avatar
+  has_one_attached :avatar, dependent: :destroy
 
 end
